d71489db63f1c6c0f6410d64b5bdbdebd0068a67
Javier Fernández <jfernandil@alumnos.unex.es>
2019-02-13 18:14:44 +0100

    Trying different regex to parse the input file

--- examples/filesRE.py
+++ examples/filesRE.py
@@ -2,10 +2,8 @@ import click
 import csv
 import re
 
-# first regex try
-#rex = re.compile('(?!(^[0-9]+:$))(?!^$)')
-# IRC #regex suggestion
-rex = re.compile('(?<!\S)\d+(?!\S)')
+
+rex = re.compile('(?!(^[0-9]+:$))(?!^$)')
 
 @click.command()
 @click.argument('input', type=click.File('r'))
@@ -17,7 +15,6 @@ def parser(input, output):
         next(input)
     i = 1
     reader = csv.reader(input, delimiter=' ')
-    output.write('channel,count\n')
     for row in reader:
         for r in row:
             if rex.match(r):
