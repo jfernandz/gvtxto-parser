30e842abd7045ab7131221bb5a17c8c60c2fb51c
Javier Fernández <jfernandil@alumnos.unex.es>
2019-02-13 20:21:19 +0100

    CSV module has been removed
    Instead of CSV module we are using regex module. I think this is  much 
    more powerful to parse raw data. I highly recommend regex101.com to test 
    patterns and regular expressions.

--- gvtxto-parser.py
+++ gvtxto-parser.py
@@ -1,28 +1,31 @@
 import click
-import csv
 import re
+
 
 # first regex try
 #rex = re.compile('(?!(^[0-9]+:$))(?!^$)')
-# IRC #regex suggestion
-rex = re.compile('(?<!\S)\d+(?!\S)')
+# IRC #regex suggestion; regex101.com might be helpful
+rexdata = r"(?<!\S)\d+(?!\S)"
+rexinfo = r"^(?:(?! *\d+:).*\n)*"
 
 @click.command()
 @click.argument('input', type=click.File('r'))
 @click.argument('output', type=click.File('w'))
+
 def parser(input, output):
+    i_str = input.read()
+    #print(i_str)
     # Parsing
     # parse input file and writing an output file
-    for _ in range(4):
-        next(input)
-    i = 1
-    reader = csv.reader(input, delimiter=' ')
-    output.write('channel,count\n')
-    for row in reader:
-        for r in row:
-            if rex.match(r):
-                output.write('{},{} \n'.format(i, r))
-                i = i + 1
+    info_matches = re.search(rexinfo, i_str)
+    if info_matches:
+        #print("{match}".format(match=info_matches.group()))
+        output.write('{match}\nchannel,count\n'.format(match=info_matches.group()))
+        noinf_str = re.sub(rexinfo, ' ', i_str)
+    data_matches = re.finditer(rexdata, noinf_str)
+    for i, match in enumerate(data_matches, start=1):
+        #print("{i} {match}".format(i=i, match=match.group()))
+        output.write('{i},{match}\n'.format(i=i, match=match.group()))
 
 if __name__ == '__main__':
     parser()
