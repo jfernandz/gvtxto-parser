48315feb18bb2b1d54a09fa8a4ad07ee9b1a0cda
Javier Fernández <jfernandil@alumnos.unex.es>
2019-02-15 18:22:27 +0100

    Enhancements
    Arguments use has been abandoned, instead of I'm using options 
    exclusively. This way I'm able to turn on the prompt input mode (with no 
    autocomplete feature, although).

--- gvtxto-parser.py
+++ gvtxto-parser.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import click
 import re
 # first regex try
@@ -5,14 +7,47 @@ import re
 # IRC #regex suggestion; regex101.com might be helpful
 rexinfo = r"^(?:(?! *\d+:).*\n)*"
 rexdata = r"(?<!\S)\d+(?!\S)"
+
+
 # Fetching command line arguments, with click module
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo('Version 1.0')
+    ctx.exit()
+
+
 @click.command()
-@click.option('--prompt/--no-prompt', '-p/ ', default=False,
-              help='Enables prompt input mode')
-@click.option('--head/--no-head', ' /-H', default=True,
-              help='Enables/disables header printing')
+@click.option('--version',
+              '-v',
+              is_flag=True,
+              callback=print_version,
+              expose_value=False,
+              is_eager=True,
+              help="Prints this script's version")
+@click.option(
+    '--input',
+    '-i',
+    prompt=True,
+    type=click.File('r'),
+    help='Input file; absolute/relative path')
+@click.option(
+    '--output',
+    '-o',
+    prompt=True,
+    type=click.File('w'),
+    help="Output file (you'll must add the file extenxion)")
+@click.option(
+    '--out-type',
+    type=click.Choice(['raw', 'enhanced', 'treatable']),
+    default='enhanced',
+    help=('raw:\nonly count per channel data \n'
+          'enhanced: including GammaVision log info \n'
+          'tratable: csv by comma separated\nwith columns header'
+          ),
+)
 # Core function. It is parsing the input file and writing an output new file
-def parser(input, output, head):
+def parser(input, output, out_type, version):
     """
     This script allows you to convert GammaVision .txt output files into a
     much more treatable format, printing just one channel (and its respective
@@ -21,7 +56,7 @@ def parser(input, output, head):
     It has no options for the momment (in addition to --help), maybe in a
     future. A practical usage would be:
 
-            $ python gvtxto-parser.py <inputfile.txt> <outputfile.dat>
+            $ python gvtxto-parser.py [options] -i <inpfil.txt> -o <outfil.dat>
 
     Theoretically you are going to be able to choose the format what you
     want (.dat in this case).
@@ -47,20 +82,27 @@ def parser(input, output, head):
     if info_matches:
         # print("{match}".format(match=info_matches.group()))
         # include metadata instead write in the file itself ?
-        if head:
-            print('hello')
-            output.write('{match}\n'.format(match=info_matches.group()))
-        output.write('channel,count\n')
-
         noinf_str = re.sub(rexinfo, ' ', i_str)
-    # Second match created, using re.finditer() to iterate the searching
-    # process. This iterative behavior is also useful to print the channel
-    # number alongside his count, that's why we have used 'i' index and
-    # enumerate()
-    data_matches = re.finditer(rexdata, noinf_str)
-    for i, match in enumerate(data_matches, start=1):
-        # print("{i} {match}".format(i=i, match=match.group()))
-        output.write('{i},{match}\n'.format(i=i, match=match.group()))
+        # Second match created, using re.finditer() to iterate the
+        # searching process. This iterative behavior is also useful
+        # to print the channel number alongside his count, that's why
+        # we have used 'i' index and enumerate()
+        data_matches = re.finditer(rexdata, noinf_str)
+        if out_type == 'enhanced':
+            output.write('{match}\n'.format(match=info_matches.group()))
+            output.write('channel,count\n')
+            for i, match in enumerate(data_matches, start=1):
+                # print("{i} {match}".format(i=i, match=match.group()))
+                output.write('{i},{match}\n'.format(i=i, match=match.group()))
+        elif out_type == 'treatable':
+            output.write('channel,count\n')
+            for i, match in enumerate(data_matches, start=1):
+                # print("{i} {match}".format(i=i, match=match.group()))
+                output.write('{i},{match}\n'.format(i=i, match=match.group()))
+        elif out_type == 'raw':
+            for i, match in enumerate(data_matches, start=1):
+                # print("{i} {match}".format(i=i, match=match.group()))
+                output.write('{match}\n'.format(match=match.group()))
 
 
 if __name__ == '__main__':
