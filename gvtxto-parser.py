5198fc52fb7ee0b9fc3f2eb2715a7c8d6be0a7c1
Javier <jfernandil@alumnos.unex.es>
2019-02-19 20:34:48 +0100

    Raw mode fixed
    I'd forgotten to add breakline '\n' in raw output mode

--- gvtxto-parser.py
+++ gvtxto-parser.py
@@ -105,7 +105,7 @@ def parser(input, output, out_type):
         elif out_type == 'raw':
             for i, match in enumerate(data_matches, start=1):
                 # print("{i} {match}".format(i=i, match=match.group()))
-                output.write('{match}'.format(match=match.group()))
+                output.write('{match}\n'.format(match=match.group()))
         elif out_type == 'csv':
             # writing columns headers. Using csv module
             col_nam = ['channel', 'count']
